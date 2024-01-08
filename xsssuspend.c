00fac6ee2548aa7e8f7be21dce51b4c4e4e51875
Vadim Vygonets <vadik@vygo.net>
2024-01-08 20:05:15 +0100

    LICENSE, xsssuspend: Update years in license

--- xsssuspend.c
+++ xsssuspend.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Vadim Vygonets <vadik@vygo.net>
+ * Copyright (c) 2022, 2024 Vadim Vygonets <vadik@vygo.net>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
